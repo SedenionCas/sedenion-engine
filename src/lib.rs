8c050739b75e98b4db43991ce54a5eead2918114
Anto Keinänen <62938747+antoKeinanen@users.noreply.github.com>
2023-08-18 16:47:15 +0300

    feat: better error handling (#8)
    * feat: better error handling
    
    * feat: display error handling in example

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 use wasm_bindgen::prelude::*;
 
-mod math;
 mod error;
-mod parser;
-mod optimizer;
+mod math;
 pub mod numeric_evaluator;
+mod optimizer;
+mod parser;
 
 #[cfg(test)]
 mod tests;
 
-
 #[wasm_bindgen]
-pub fn evaluate(expression: &str) -> f64 {
-    numeric_evaluator::evaluate(expression).unwrap()
+pub fn evaluate(expression: &str) -> Result<f64, String> {
+    match numeric_evaluator::evaluate(expression) {
+        Ok(val) => Ok(val),
+        Err(err) => Err(err.to_string()),
+    }
 }
