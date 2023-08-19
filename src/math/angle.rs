be07e35b5791a143bca3f2521cde0f9b5197f28f
antoKeinanen <anto.keinanen@gmail.com>
2023-08-19 18:52:28 +0300

    feat: add radian to degree converter

--- src/math/angle.rs
+++ src/math/angle.rs
@@ -3,3 +3,7 @@ use std::f64::consts::PI;
 pub fn deg_to_rad(a: f64) -> f64 {
     a*(PI/180.0)
 }
+
+pub fn rad_to_deg(x: f64) -> f64 {
+    x*(180.0/PI)
+}
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -4,5 +4,6 @@ mod constants;
 
 pub use round::round;
 pub use angle::deg_to_rad;
+pub use angle::rad_to_deg;
 
 pub use constants::CONSTANTS_DATABASE;
