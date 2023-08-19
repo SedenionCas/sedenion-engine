25bf4bcaf8a62756509794b5dab4bc4aab1ee991
antoKeinanen <anto.keinanen@gmail.com>
2023-08-19 18:53:13 +0300

    test: write tests for angle conversions

--- src/math/angle.rs
+++ src/math/angle.rs
@@ -7,3 +7,24 @@ pub fn deg_to_rad(a: f64) -> f64 {
 pub fn rad_to_deg(x: f64) -> f64 {
     x*(180.0/PI)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::f64::consts::PI;
+
+    use crate::math::{deg_to_rad, angle::rad_to_deg};
+
+    #[test]
+    fn can_convert_degrees_to_radians() {
+        assert_eq!(PI/2.0, deg_to_rad(90.0));
+        assert_eq!(PI, deg_to_rad(180.0));
+        assert_eq!(2.0*PI, deg_to_rad(360.0));
+    }
+
+    #[test]
+    fn can_convert_radians_to_degrees() {
+        assert_eq!(90.0, rad_to_deg(PI/2.0));
+        assert_eq!(180.0, rad_to_deg(PI));
+        assert_eq!(360.0, rad_to_deg(2.0*PI));
+    }
+}
