39dbac54c51090d8f2b5cf01a29441bbe1c490bb
antoKeinanen <anto.keinanen@gmail.com>
2023-09-07 09:53:00 +0300

    ref: make monomials have lowercase variable and constants have uppercase

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -3,8 +3,8 @@ use std::f64::consts::{E, PI, TAU};
 use phf::phf_map;
 
 pub static CONSTANTS_DATABASE: phf::Map<&'static str, f64> = phf_map! {
-    "pi" => PI,
-    "tau" => TAU,
-    "e" => E,
-    "phi" => 1.618_033_988_749_895,
+    "PI" => PI,
+    "TAU" => TAU,
+    "E" => E,
+    "PHI" => 1.618_033_988_749_895,
 };
